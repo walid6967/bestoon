bd13e2061a4a87b26a3b6c59259f775ebf3868eb
walid6967 <waliakrami121@gmail.com>
2024-10-12 16:54:04 +0430

    add requirements.txt

--- web/models.py
+++ web/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date =  models.DateTimeField()
     amount = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def get_date(self):
+        return self.date
+
+    def __str__(self):
+        return f"{self.text} - {self.amount} on {self.date}"
+
 class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_date(self):
+        return self.date
+    
+    def __str__(self):
+        return f"{self.text} - {self.amount} on {self.date}"
