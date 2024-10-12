dd18cc1d34c3cc06b511f6efe44f1f95949ce269
walid6967 <waliakrami121@gmail.com>
2024-10-12 15:55:14 +0430

    create Expense model

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Expense
 
+admin.site.register(Expense)
 # Register your models here.
--- web/models.py
+++ web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date =  models.DateTimeField()
+    amount = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
